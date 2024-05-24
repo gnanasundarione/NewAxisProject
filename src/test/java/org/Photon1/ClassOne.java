29cbfc4381ca4ffc4b31e13763efcc9aa58b51a0
gnanasundarione <gavoury.4g@gmail.com>
2024-05-24 20:29:19 +0530

    First time Commit

---
+++ src/test/java/org/Photon1/ClassOne.java
@@ -0,0 +1,22 @@
+package org.Photon1;
+
+public class ClassOne {
+	
+	private void method1() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void method2() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void method3() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+
+}
