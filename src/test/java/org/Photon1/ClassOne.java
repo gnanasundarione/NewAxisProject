0199474f74600007e4206323464846eeaa3c2d24
gnanasundarione <gavoury.4g@gmail.com>
2024-05-25 23:29:31 +0530

    Gnans User commit

--- src/test/java/org/Photon1/ClassOne.java
+++ src/test/java/org/Photon1/ClassOne.java
@@ -3,19 +3,33 @@ package org.Photon1;
 public class ClassOne {
 	
 	private void method1() {
-		// TODO Auto-generated method stub
+		System.out.println("Method one");
 
 	}
 	
 	private void method2() {
-		// TODO Auto-generated method stub
-
+		System.out.println("Method Two");
 	}
 	
 	private void method3() {
-		// TODO Auto-generated method stub
-
+		System.out.println("Method three");
 	}
+	
+	private void methodGreens1() {
+		
+		System.out.println("Method Greens One");
+	}
+	
+	private void methodGreens2() {
+		System.out.println("Method Greens Two");
+	}
+	
+	private void methodGreens3() {
+		
+		System.out.println("Method Greens three");
+	}
+	
+	
 	
 	
 
